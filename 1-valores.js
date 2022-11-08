0bf473beca77792402e40dfff76354b0fbcb41bb
Diegomez-dev <diegoagc@yahoo.es>
2022-11-08 17:34:08 -0500

    initial commit

---
+++ 1-valores.js
@@ -0,0 +1,31 @@
+// Tipos primitivos
+
+// Numeros 
+40   
+3.14
+
+// Strings (cadenas de texto), Esto es la forma en la que puedo crear algo que se pueda imprimir en la pantalla, o escribir información, que el usuario pueda entender y leer. utilizar palabras, o frases.
+ "Diego De Granda"  
+
+ 'Diego De Granda'
+
+ // Booleanos, estos fueron inventados por George Boole, un Matemático británico. y esto hace referencia a cosas que podamos validar.
+ true   
+ false
+
+ // Valores vacios (Empty values), es como un placeholder (espacio reservado para algun valor) de algún valor faltante.
+ 
+ null  
+ undefined
+
+
+ /* ================================================================ */ 
+ 
+ // Tipos de objetos o no primitivos
+
+// corchetes // arrays
+ [1, 2, 3, 4] 
+
+ // llaves // objetos
+ { nombre: 'Diego' } 
+
